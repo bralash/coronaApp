d60778f1ae764aa02c0bf722e85ef628193ab20c
bralash <emmanuelasaber@gmail.com>
2020-05-06 16:51:08 +0000

    Add info screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:coronaapp/constant.dart';
+import 'package:coronaapp/info_screen.dart';
+import 'package:coronaapp/widgets/counter.dart';
+import 'package:coronaapp/widgets/my_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/services.dart';
@@ -107,58 +110,11 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            ClipPath(
-              clipper: MyClipper(),
-              child: Container(
-                padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-                height: 350,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [
-                      Color(0xFF3383CD),
-                      Color(0xFF11249F),
-                    ],
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/virus.png"),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: SvgPicture.asset("assets/icons/menu.svg"),
-                    ),
-                    SizedBox(height: 20,),
-                    Expanded(
-                      child: Stack(
-                        children: <Widget>[
-                          SvgPicture.asset("assets/icons/Drcorona.svg",
-                          width: 230,
-                          fit: BoxFit.fitWidth,
-                          alignment: Alignment.topCenter,
-                          ),
-                          Positioned(
-                            top: 20,
-                            left: 150,
-                            child: Text("All you need \nis to stay home", 
-                              style: kHeadingTextStyle.copyWith(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                          Container(), 
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+            MyHeader(
+              textTop: "All you need",
+              textBottom: "is to stay home",
+              image: "assets/icons/Drcorona.svg",
               ),
-            ),
             Container(
               margin: EdgeInsets.symmetric(horizontal: 20),
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
@@ -316,75 +272,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-
-
-class Counter extends StatelessWidget {
-  final int number;
-  final Color color;
-  final String title;
-  
-  const Counter({
-    Key key,
-    this.number, 
-    this.color, 
-    this.title,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(6),
-          height: 25,
-          width: 25,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: color.withOpacity(.26),
-          ),
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.transparent,
-              border: Border.all(
-                color: color,
-                width: 2,
-              ),
-            ),
-          ),
-        ),
-        SizedBox(height: 10),
-        Text(
-          "$number", 
-          style: TextStyle(
-            fontSize: 30,
-            color: color,
-          ),
-        ),
-        Text(title, style: kSubTextStyle),
-      ],
-    );
-  }
-}
-
-
-class MyClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(size.width/2, size.height, size.width, size.height - 80);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
-  }
-}
-
-
-  
